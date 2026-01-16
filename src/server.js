f2dc52c6e21f81a99c7e6934d3b3c7cbcce0fc47
ezeta-03 <ing.sergio.zenteno@gmail.com>
2026-01-16 16:32:50 -0500

    REFACTOR: Change Mysql to Postgres

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
-
-
 const app = require("./App");
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto", PORT);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
